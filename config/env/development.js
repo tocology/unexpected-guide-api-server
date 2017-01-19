ea34b79175b979b3990b2de32a4a54bc1fcbd97b
진형탁 <njir@naver.com>
2017-01-19 20:52:05 +0900

    chore: Modify db config

--- config/env/development.js
+++ config/env/development.js
@@ -4,28 +4,10 @@ export default {
     jwtSecret: 'besoul-api-development',
     port: 3000,
     // for MySQL setup
-    /*
     db: {
         database: 'besoul',
         user: 'successnicely',
         password: 'gangnamtoz!@#',
-        options: {
-            host: 'localhost',
-            dialect: 'mysql',
-
-            pool: {
-                max: 100,
-                min: 0,
-                idle: 10000
-            }
-        }
-    }
-*/
-
-    db: {
-        database: 'docent_dev',
-        user: 'docent_user',
-        password: '',
         options: {
             host: 'localhost',
             dialect: 'mysql',
