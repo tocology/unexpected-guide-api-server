b37fb95428e6033a254d349bd760fe06f6f8125e
Camry(황준호) <tocology@coupang.com>
2017-04-09 12:08:41 +0900

    feat: modify for new domain

--- migration/mysql/create_user.sql
+++ migration/mysql/create_user.sql
@@ -4,10 +4,10 @@ CREATE USER 'succeednicely'@'%' IDENTIFIED BY 'failnicely';
 USE mysql;
 
 -- DB 별 권한 부여
-GRANT DELETE ON unexpectedguide.* TO 'succeednicely'@'%';
-GRANT INSERT ON unexpectedguide.* TO 'succeednicely'@'%';
-GRANT SELECT ON unexpectedguide.* TO 'succeednicely'@'%';
-GRANT UPDATE ON unexpectedguide.* TO 'succeednicely'@'%';
+GRANT DELETE ON uxguide.* TO 'succeednicely'@'%';
+GRANT INSERT ON uxguide.* TO 'succeednicely'@'%';
+GRANT SELECT ON uxguide.* TO 'succeednicely'@'%';
+GRANT UPDATE ON uxguide.* TO 'succeednicely'@'%';
 
 -- 마무리
 FLUSH PRIVILEGES;
