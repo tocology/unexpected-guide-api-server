5db5be80d29d95a0c828a6200d9322c5c7218d67
Camry(황준호) <tocology@coupang.com>
2017-05-07 10:17:43 +0900

    feat: Add more features

--- src/helpers/CloudFront.js
+++ src/helpers/CloudFront.js
@@ -4,16 +4,18 @@ import path from 'path';
 
 const cfPath = 'http://d3gr0p3062v2h3.cloudfront.net/';
 
-const signingParams = {
-  keypairId: 'APKAITG2XNVB7FJJ53ZA',
-  // privateKeyString: process.env.PRIVATE_KEY,
-  // Optional - this can be used as an alternative to privateKeyString
-  privateKeyPath: path.resolve(__dirname, 'cloudfront/pk-APKAITG2XNVB7FJJ53ZA.pem'),
-  expireTime: moment().utc().add(1, 'day')
+function _generateSigningParams() {
+  return {
+    keypairId: 'APKAITG2XNVB7FJJ53ZA',
+    // privateKeyString: process.env.PRIVATE_KEY,
+    // Optional - this can be used as an alternative to privateKeyString
+    privateKeyPath: path.resolve(__dirname, 'cloudfront/pk-APKAITG2XNVB7FJJ53ZA.pem'),
+    expireTime: moment().utc().add(1, 'day')
+  }
 }
 
 function getSignedUrl(bucketKey) {
-  return cfsign.getSignedUrl(cfPath + bucketKey, signingParams);
+  return cfsign.getSignedUrl(cfPath + bucketKey, _generateSigningParams());
 }
 
 export default {
